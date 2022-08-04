ca8df25d13285003730907452ac3d6a03ffa629b
chenxing <chenxing@bookan.com.cn>
2022-08-04 17:46:05 +0800

    c and go

--- c-et-lt/lt.c
+++ c-et-lt/lt.c
@@ -22,18 +22,18 @@ void lib_epoll_mod(int op, int fd);
 int lib_epoll_wait(struct epoll_event *events, int num, int timeout);
 
 int main(int argc, char *argv[]) {
-    char *str = "GET / HTTP/1.1\n\n";
-    int clientFd = socket(PF_INET, SOCK_STREAM, 0);
-    lib_connect(clientFd, "localhost", 80);
-    int ret = write(clientFd, str, strlen(str));
+    char *str = "ping \r\n";
+    int client_fd = socket(PF_INET, SOCK_STREAM, 0);
+    lib_connect(client_fd, "localhost", 6379);
+    int ret = write(client_fd, str, strlen(str));
 
     lib_epoll_create1();
-    lib_epoll_ctl(EPOLL_CTL_ADD, clientFd, EPOLLIN | EPOLLONESHOT);
+    lib_epoll_ctl(EPOLL_CTL_ADD, client_fd, EPOLLIN);
     struct epoll_event *events;
     events = calloc(64, sizeof(struct epoll_event));
     int num;
     num = lib_epoll_wait(events, 64, 1000);
-    printf("num:%d events:%d\n", num, events[0].events);
-    num = lib_epoll_wait(events, 64);
-    printf("num:%d events:%d\n", num, events[0].events);
+    printf("num:%d events:%d\n", num, events[0].data.fd);
+    num = lib_epoll_wait(events, 64, 1000);
+    printf("num:%d events:%d\n", num, events[0].data.fd);
 }
