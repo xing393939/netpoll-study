22ec5a69236601ffac3f09e74ed44e6550ac5da0
chenxing <chenxing@bookan.com.cn>
2022-08-07 15:44:25 +0800

    c and go

---
+++ go.mod
@@ -0,0 +1,5 @@
+module netpoll-study
+
+go 1.16
+
+require github.com/cloudwego/netpoll v0.2.6 // indirect
---
+++ netpool-redis-server/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+	"github.com/cloudwego/netpoll"
+)
+
+func main() {
+	listener, _ := netpoll.CreateListener("tcp", "localhost:6382")
+	eventLoop, _ := netpoll.NewEventLoop(
+		func(ctx context.Context, connection netpoll.Connection) error {
+			reader, writer := connection.Reader(), connection.Writer()
+			msg, _ := reader.ReadString(reader.Len())
+			if msg != "" {
+				_ = reader.Release()
+				_, _ = writer.WriteString("+PONG\r\n")
+				_ = writer.Flush()
+			}
+			return nil
+		},
+	)
+	_ = eventLoop.Serve(listener)
+}
