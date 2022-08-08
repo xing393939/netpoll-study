ac690414ad092a46ed1ef3e8238f3dd4c54de26d
chenxing <chenxing@bookan.com.cn>
2022-08-08 18:16:21 +0800

    c and go

---
+++ c-et-lt/2.map_write.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/epoll.h>
+#include <netdb.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <time.h>
+#include <signal.h>
+#include <string.h>
+
+void lib_connect(int fd, char *addr, int port);
+
+void lib_epoll_create1();
+
+void lib_epoll_ctl(int op, int fd, int events);
+
+void lib_epoll_mod(int op, int fd);
+
+int lib_epoll_wait(struct epoll_event *events, int num, int timeout);
+
+int main(int argc, char *argv[]) {
+    int client_fd = socket(PF_INET, SOCK_STREAM, 0);
+    lib_connect(client_fd, "localhost", 6379);
+
+    int f = open("ping.txt", O_RDONLY);
+    char *str;
+
+    for (int i = 0; i < 10000; i++) {
+        str = mmap(0, 10, PROT_READ, MAP_SHARED, f, 0);
+        write(client_fd, str, strlen(str));
+    }
+}
